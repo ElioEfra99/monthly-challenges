e3571c97f92127b88fef2a72d319c6b7dd0defea
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2021-06-24 16:35:45 -0500

    Rendering Templates

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
+# from django.template.loader import render_to_string
 
 month_challenges = {
     'january': 'Eat no meat for the entire month',
@@ -35,9 +35,14 @@ def index(request):
 def monthly_challenge(request, month):
     try:
         challenge_text = month_challenges[month]
-        response_data = render_to_string('challenges/challenge.html')
 
-        return HttpResponse(response_data)
+        # response_data = render_to_string('challenges/challenge.html')
+        # return HttpResponse(response_data)
+
+        # The code line above ⬆️ equals to the code line bellow ⬇️
+
+        return render(request, 'challenges/challenge.html')
+
     except:
         return HttpResponseNotFound('This month is not supported')
 
